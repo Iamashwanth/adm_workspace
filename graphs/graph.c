a60ec26d73b114faccb12aea7850864b62992812
Iamashwanth <iamashwanth@gmail.com>
2017-03-03 00:32:19 +0530

    graph: Inital version of BFS

--- graphs/graph.c
+++ graphs/graph.c
@@ -5,11 +5,27 @@
 
 #define MAX_V 100
 
+typedef enum {
+	UNDISCOVERED,
+	DISCOVERED,
+	PROCESSED
+} edgestate;
+
+typedef struct listnode {
+	int elem;
+	struct listnode *next;
+} listnode;
+
 typedef struct edgenode {
 	int y;
 	int weight;
 	struct edgenode *next;
 } edgenode;
+
+typedef struct {
+	listnode *head;
+	listnode *tail;
+} queue;
 
 typedef struct {
 	int nvertices;
@@ -18,6 +34,49 @@ typedef struct {
 	edgenode *edges[MAX_V];
 	int degree[MAX_V];
 } graph;
+
+void initQueue (queue *q) {
+	q->head = q->tail = NULL;
+}
+
+int isEmpty (queue *q) {
+	if (q->head == NULL || q->tail == NULL) return 1;
+	else return 0;
+}
+
+void enqueue (queue *q, int elem) {
+	listnode *node;
+	node = (listnode *) malloc(sizeof(listnode));
+	node->elem = elem;
+	node->next = q->tail;
+	q->tail = node;
+
+	if (q->head == NULL) {
+		q->head = node;
+	}
+}
+
+int dequeue (queue *q) {
+	listnode *iter, *node;
+	int elem;
+
+	if (q->head == q->tail) {
+		node = q->head;
+		q->head = q->tail = NULL;
+	} else {
+		iter = q->tail;
+		while (iter->next != q->head) {
+			iter = iter->next;
+		}
+		node = q->head;
+		q->head = iter;
+	}
+
+	elem = node->elem;
+	free(node);
+
+	return elem;
+}
 
 void initGraph (graph *g, bool directed) {
 	int i ;
@@ -78,9 +137,46 @@ void printGraph (graph* g) {
 	}
 }
 
+void BFS (graph *g, int start) {
+	int x, *parent;
+	edgenode *temp;
+	edgestate *state;
+	queue q;
+
+	state = (edgestate *) calloc(g->nvertices, sizeof(edgestate));
+	parent = (int *) calloc(g->nvertices, sizeof(int));
+
+	state[start] = DISCOVERED;
+
+	initQueue(&q);
+	enqueue(&q, start);
+
+	while (!isEmpty(&q)) {
+		x = dequeue(&q);
+		printf("Processing vertex %d\n", x);
+
+		temp = g->edges[x];
+
+		while (temp != NULL) {
+			if (state[temp->y] == UNDISCOVERED) {
+				state[temp->y] = DISCOVERED;
+				parent[temp->y] = x;
+				enqueue(&q , temp->y);
+			}
+			temp = temp->next;
+		}
+
+		state[x] = PROCESSED;
+	}
+}
+
 int main() {
+	int x;
 	graph g;
 	initGraph(&g, false);
 	readGraph(&g);
 	printGraph(&g);
+	printf("enter the first vertex to start transversal\n");
+	scanf("%d", &x);
+	BFS(&g, x);
 }
